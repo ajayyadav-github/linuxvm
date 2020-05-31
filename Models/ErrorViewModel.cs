8fb97fab7a982299a7de8c7ca747e082aad5de3a
Ajay yadav <ajayyadav271295@gmail.com>
2020-05-31 21:54:06 +0530

    1st

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestCoremvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
